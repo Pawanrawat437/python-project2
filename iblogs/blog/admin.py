467eba653542f6d45e6ebc54498b155a605a338e
Pawanrawat437 <95847670+Pawanrawat437@users.noreply.github.com>
2022-03-14 12:11:52 +0530

    Add files via upload

---
+++ iblogs/blog/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Category,Post
+
+# Register your models here.
+#for configuration of  category admin
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('image_tag','title','description','url','add_date')
+    search_fields = ('title',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_diplay = ('title',)
+    search_fields = ('title',)
+    list_filter = ('cat',)
+    list_per_page = 50
+
+    class Media:
+        js = ('<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>'
+'js/script.js',)
+
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Post)
+
